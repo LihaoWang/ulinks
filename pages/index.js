f2597f4ef3896fcdf3b096f0255730729f314986
leowang <leowang9863@gmail.com>
2021-08-13 23:25:58 -0400

    remove loader

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import Loader from "../components/Loader";
+
 import toast from "react-hot-toast";
 export default function Home() {
   return <div></div>;
